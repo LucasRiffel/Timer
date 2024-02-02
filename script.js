03f75d058ce4cd234ea6d2c9049a855486103360
Lucas Riffel <lucas26.riffel@gmail.com>
2024-02-01 22:28:33 -0300

    update

--- script.js
+++ script.js
@@ -1,37 +1,58 @@
 var contagem = 0;
-var intervaloId;
+var intervalo;
+var estaContando = false;
+var estaPausado = false;
+
+function converterSegundos(segundos) {
+    var minutos = Math.floor(segundos / 60);
+    var segundosRestantes = segundos % 60;
+
+    if (segundosRestantes < 10) {
+        segundosRestantes = "0" + segundosRestantes;
+    }
+
+    return minutos + ":" + segundosRestantes;
+}
 
 document.getElementById('começar').addEventListener('click', function () {
     var tempo = document.getElementById('tempo').value;
-    if (tempo !== "" && tempo > 0) {
-        contagem = tempo;
-        intervaloId = setInterval(function () {
+    if (tempo !== "" && tempo > 0 && !estaContando) {
+        if (!estaPausado) { 
+            contagem = tempo;
+        }
+        estaContando = true;
+        estaPausado = false;
+        intervalo = setInterval(function () {
             contagem--;
-            document.getElementById('timer').innerHTML = contagem;
+            document.getElementById('timer').innerHTML = converterSegundos(contagem);
             if (contagem <= 0) {
-                clearInterval(intervaloId);
-                document.getElementById('timer').innerHTML = "Contagem terminada";
-            }  
+                clearInterval(intervalo);
+                estaContando = false;
+                document.getElementById('timer').innerHTML = "Terminada";
+            }
         }, 1000)
     }
 });
 
-document.querySelector('.botao-adicionar').addEventListener('click', function() {
+document.querySelector('.botao-adicionar').addEventListener('click', function () {
     var tempo = document.getElementById('tempo').value;
     if (tempo !== "" && tempo > 0) {
         contagem = tempo;
-        document.getElementById('timer').innerHTML = contagem;
+        document.getElementById('timer').innerHTML = converterSegundos(contagem);
     }
 });
 
-
 document.getElementById('pause').addEventListener('click', function () {
-    clearInterval(intervaloId);
+    clearInterval(intervalo);
+    estaContando = false;
+    estaPausado = true;
 });
 
 document.getElementById('reiniciar').addEventListener('click', function () {
-    clearInterval(intervaloId);
+    clearInterval(intervalo);
     contagem = 0;
-    document.getElementById('timer').innerHTML = "";
+    estaContando = false;
+    estaPausado = false;
+    document.getElementById('timer').innerHTML = "0:00";
     document.getElementById('tempo').value = "";
 });
